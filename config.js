4f45b12bd35a055c00b1749b61b4ee6304a77bac
Maiko <maikokuroki24@gmail.com>
2020-10-02 10:51:58 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '344763396607365',
+        clientSecret: '825ebdfdfcf112519b3dd30414619b7a'
+    }
 }
